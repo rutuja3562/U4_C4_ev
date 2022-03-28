24e0d0ef1534158df4460d7b1cdf4bce42104193
rutuja3562 <rspatil3562@gmail.com>
2022-03-28 10:58:18 +0530

    second commit

--- src/controllers/athcontroller.js
+++ src/controllers/athcontroller.js
@@ -1,12 +1,21 @@
 const User = "../models/user.model.js";
+var jwt = require("jsonwebtoken");
+require("dotenv").config();
+generateToken = (user)=>{
+return jwt.sign({user},process.env.secrate_key);
+}
+
 const register = async (req, res) => {
   try {
     let user = await User.findOne({ email: req.body.email });
     if (user) {
       return res.status(400).send({ massege: "email already registerd" });
     }
-    user = await User.create(req.body);
-  return res.status(200).send({ user});
+      user = await User.create(req.body);
+      const generateToken = user;
+  return res.status(200).send({ user, token});
+
+
 
   } catch (err) {
     res.status(400).send({ massege: err.mess });
@@ -19,6 +28,15 @@ const login = async (req, res) => {
     if (!user) {
       return res.status(400).send({ massege: "wrong email or password" });
     }
+ const match = user.checkPassword(req.body.password);
+ if(!match){
+ return res.status(400).send({ massege: "wrong email or password" });
+
+ }
+   user = await User.create(req.body);
+   const generateToken = user;
+   return res.status(200).send({ user, token });
+
   } catch (err) {
     res.status(400).send({ massege: err.mess });
   }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose");
 
-
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
 firstName:{type:String,required:true},
 lastName:{type:String,required:false},
@@ -12,5 +12,13 @@ password:{type:String,required:true},
     timestamps:true,
 })
 
+userSchema.pre("save", async(req,res,next)=>{
+    const hash = bcrypt.hashSync(mthis.password, 7);
+    this.password= hash;
+    next();
+})
+// userSchema.models.checkPassword = function(password){
+//     return bcrypt.compareSync(password, this.password);
+// }
 User = mongoose.model("user",userSchema);
 module.exports = User;
